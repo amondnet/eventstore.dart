d02cd087072c0c62644d5ac913eda8a176fa33bb
Minsu Lee <amond@amond.net>
2020-05-13 13:34:25 +0900

    first commit

---
+++ lib/eventstore.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library eventstore;
+
+export 'src/eventstore_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
